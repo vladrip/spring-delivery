7a8797307ae30a5367057bcbb69bfd0bf75c8c72
vladrip <vladrip1>
2022-12-15 23:03:31 +0200

    project creation

---
+++ src/main/java/com/exam/spring_delivery/SpringDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.exam.spring_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exam/spring_delivery/SpringDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.spring_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
